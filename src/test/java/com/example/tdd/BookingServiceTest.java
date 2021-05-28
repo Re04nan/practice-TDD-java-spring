a3fd6a4bb957af8241ef145a42ea923e57b646de
Re04nan <renan04_marques@hotmail.com>
2021-05-28 14:40:32 -0300

    add object booking and test booking

---
+++ src/test/java/com/example/tdd/BookingServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.tdd;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+//@RunWith(RunExtension.class) em versões do Junit inferior a 5
+
+@ExtendWith(SpringExtension.class)
+public class BookingServiceTest {
+
+}
