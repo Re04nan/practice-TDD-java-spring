2a9aeb2874da21d3eaaf67c196677997c744947b
Re04nan <renan04_marques@hotmail.com>
2021-05-28 17:42:18 -0300

    add booking service and method

---
+++ src/main/java/com/example/tdd/service/BookingService.java
@@ -0,0 +1,12 @@
+package com.example.tdd.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+
+	public int daysCalculatorWithDatabase(String name) {
+		return 10;
+	}
+
+}
--- src/test/java/com/example/tdd/BookingServiceTest.java
+++ src/test/java/com/example/tdd/BookingServiceTest.java
@@ -1,12 +1,38 @@
 package com.example.tdd;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.tdd.service.BookingService;
 
 
 //@RunWith(RunExtension.class) em versões do Junit inferior a 5
 
 @ExtendWith(SpringExtension.class)
 public class BookingServiceTest {
+	
+	@TestConfiguration
+	static class BookingServiceTestConfiguration{
+		
+		@Bean
+		public BookingService bookingService() {
+			return new BookingService();
+		}
+	}
 
+	@Autowired
+	BookingService bookingService;
+	
+	@Test
+	public void bookingTestServiceDayCalculator() {
+		String name = "Renan";
+		int days = bookingService.daysCalculatorWithDatabase(name);
+		
+		Assertions.assertEquals(days, 10);
+	}
 }
